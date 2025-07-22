d16139e5aa761aa7b9865bad5d79232ed18fe07f
Teerapong Suksuwan <trapongx@gmail.com>
2025-07-22 09:46:42 +0700

    Update test to ensure simple types are mapped directly without further facade

--- src/test/kotlin/com/runninglane/facade/test/cases/simple/DelegateWithDifferentNullabilityAndMutability.kt
+++ src/test/kotlin/com/runninglane/facade/test/cases/simple/DelegateWithDifferentNullabilityAndMutability.kt
@@ -1,7 +1,31 @@
 package com.runninglane.facade.test.cases.simple
 
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDate
+import java.util.*
+
 data class DelegateWithDifferentNullabilityAndMutability(
     val id: Long,
-    var int: Int?,
-    var long: Long
+    val boolean: Boolean,
+    val byte: Byte,
+    val short: Short,
+    val char: Char,
+    val int: Int?,
+    val long: Long,
+    val float: Float,
+    val double: Double,
+    val string: String,
+    val date: Date,
+    val localDate: LocalDate,
+    val bigInteger: BigInteger,
+    val bigDecimal: BigDecimal,
+    val blob: ByteArray,
+    val clob: String,
+    val enum: TestEnum,
+    val uuid: UUID,
+    val duration: Duration,
+    val instant: Instant
 )
---
+++ src/test/kotlin/com/runninglane/facade/test/cases/simple/DelegateWithSubsetOfProperties.kt
@@ -0,0 +1,7 @@
+package com.runninglane.facade.test.cases.simple
+
+data class DelegateWithSubsetOfProperties(
+    val id: Long,
+    var int: Int?,
+    var long: Long
+)
